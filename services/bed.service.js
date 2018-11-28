7a09a4d132703c189eedcaca198ddf7c8ed90273
edoco89 <edo1cohen@gmail.com>
2018-11-29 01:27:06 +0200

    google map integration

--- services/bed.service.js
+++ services/bed.service.js
@@ -1,15 +1,24 @@
 const mongoService = require('./mongo.service')
 const ObjectId = require('mongodb').ObjectId;
 
-function query({ byCountry = '', byCity = '', type = 'rating', order = 1, accessibility = false,
+function query({ byLat = 0, byLng = 0, type = 'rating', order = 1, accessibility = false,
     wifi = false, acceptsPets = false, airConditioner = false, shampoo = false, parking = false }) {
     const sortBy = { type, order: +order }
     const filterByAmeneties = { accessibility, wifi, acceptsPets, airConditioner, shampoo, parking }
+    // _getDistanceFromLatLonInKm(byLat,byLng,lat2,lon2)
+    console.log(byLat, byLng);
+
     const queryObj = {
-        $and: [
-            { $or: [{ 'location.country': { $regex: `.*${byCountry.toLowerCase()}.*` } }, { 'location.country': { $regex: `.*${byCountry.toUpperCase()}.*` } }] },
-            { $or: [{ 'location.city': { $regex: `.*${byCity.toLowerCase()}.*` } }, { 'location.city': { $regex: `.*${byCity.toUpperCase()}.*` } }] }
-        ]
+            $and: [
+                {'location.coords': {
+                    $near: {
+                        $geometry:
+                            { type: "Point", coordinates: [+byLng, +byLat] }, $maxDistance: 20000
+                    }
+                }
+                },
+            ]
+
     }
     for (const key in filterByAmeneties) {
         if (JSON.parse(filterByAmeneties[key])) {
@@ -19,10 +28,10 @@ function query({ byCountry = '', byCity = '', type = 'rating', order = 1, access
     return mongoService.connectToDb()
         .then(dbConn => {
             const bedCollection = dbConn.collection('bed');
+            bedCollection.createIndex({ 'location.coords': "2dsphere" });
             return bedCollection.find(queryObj).sort({ [sortBy.type]: sortBy.order }).toArray()
         })
 }
-
 function getById(bedId) {
     bedId = new ObjectId(bedId)
     return mongoService.connectToDb()
@@ -46,3 +55,20 @@ module.exports = {
     remove
 }
 
+function _getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = _deg2rad(lat2 - lat1);  // deg2rad below
+    var dLon = _deg2rad(lon2 - lon1);
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2)
+        ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function _deg2rad(deg) {
+    return deg * (Math.PI / 180)
+}
