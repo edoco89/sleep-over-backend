6b54db58358276cc852aa9cfa004c2609c2d086b
edoco89 <edo1cohen@gmail.com>
2018-11-28 08:52:22 +0200

    good morning

--- services/mongo.service.js
+++ services/mongo.service.js
@@ -11,7 +11,7 @@ var dbConnection = null;
 function connectToDb() {
     if (dbConnection) return Promise.resolve(dbConnection);
     return new Promise((resolve, reject)=>{
-        MongoClient.connect(url, (err, client) => {
+         MongoClient.connect(url , (err, client) => {
             if (err) return reject('Cannot connect to Mongo');
             console.log("Connected successfully to server");
             dbConnection = client.db(dbName);
