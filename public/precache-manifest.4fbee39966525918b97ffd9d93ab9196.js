27dc85669e00c5fec52576fae4e739835ae6a554
Jake Collins <jake.james.collins@hotmail.com>
2018-12-12 16:25:29 +0200

    new public

---
+++ public/precache-manifest.4fbee39966525918b97ffd9d93ab9196.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "6258ebe97852fd4411b2",
+    "url": "/js/about.dbc2d5ba.js"
+  },
+  {
+    "revision": "17f2fcece02d233f9798",
+    "url": "/css/app.9ee34b1a.css"
+  },
+  {
+    "revision": "17f2fcece02d233f9798",
+    "url": "/js/app.489e621c.js"
+  },
+  {
+    "revision": "b347166a9c598b318dfa",
+    "url": "/css/chunk-vendors.9ea9571e.css"
+  },
+  {
+    "revision": "b347166a9c598b318dfa",
+    "url": "/js/chunk-vendors.eebaeb30.js"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "e3011357d7589a624bcd22fa71aaa9ba",
+    "url": "/img/zoey.e3011357.jpeg"
+  },
+  {
+    "revision": "60d8c36f53a3f5ded57c40eceefe4944",
+    "url": "/img/ron-wendy.60d8c36f.jpg"
+  },
+  {
+    "revision": "01947bd80724aefcb62c4b378424a20f",
+    "url": "/img/foodies.01947bd8.jpg"
+  },
+  {
+    "revision": "fa6f2eead5a88623c047549e2b3b3271",
+    "url": "/img/outdoors.fa6f2eea.jpg"
+  },
+  {
+    "revision": "9455587166019c17ff038d5517f9e81b",
+    "url": "/img/sports.94555871.jpg"
+  },
+  {
+    "revision": "64bcfe1f13fce38e2ea26280157ff2ce",
+    "url": "/fonts/Handlee-Regular.64bcfe1f.ttf"
+  },
+  {
+    "revision": "a57bf31d5e94a758c57f328e9ca3270f",
+    "url": "/fonts/Cookie-Regular.a57bf31d.ttf"
+  },
+  {
+    "revision": "25d5fb55628b86b8a752525e18da3342",
+    "url": "/fonts/GloriaHallelujah.25d5fb55.ttf"
+  },
+  {
+    "revision": "78132c6450403faefc3f440b2bb3b24a",
+    "url": "/fonts/DancingScript-Regular.78132c64.ttf"
+  },
+  {
+    "revision": "909ea5323764b3b3a2bfaec08e035a4e",
+    "url": "/fonts/Courgette-Regular.909ea532.ttf"
+  },
+  {
+    "revision": "575e4317521b381ac94c0c8207c81979",
+    "url": "/fonts/Raleway-Bold.575e4317.ttf"
+  },
+  {
+    "revision": "b1bdea561f247adc2c904f5b24a07c51",
+    "url": "/fonts/Raleway-Light.b1bdea56.ttf"
+  },
+  {
+    "revision": "5771ceaf12961b3c26914a70e18dcca8",
+    "url": "/fonts/PatrickHand-Regular.5771ceaf.ttf"
+  },
+  {
+    "revision": "324944d6dae53ea48a648b1d44ec78c3",
+    "url": "/fonts/Pacifico-Regular.324944d6.ttf"
+  },
+  {
+    "revision": "b5f80efdbd1060aa72ccf521b203df23",
+    "url": "/img/balcony-seat.b5f80efd.jpg"
+  },
+  {
+    "revision": "f4ed771d41829c3a59a27983e90e1c2f",
+    "url": "/img/party.f4ed771d.jpg"
+  },
+  {
+    "revision": "421deeed2639211d7ab3c37e91e66f60",
+    "url": "/img/john.421deeed.jpeg"
+  },
+  {
+    "revision": "42fdb45ac4b15183bf4bcbd9a99f19cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f57640df52bd1ad6ad0f7a63eb0032b1",
+    "url": "/img/friends-sofa.jpg"
+  },
+  {
+    "revision": "344fe821fe549832a7e36eabe4b728f8",
+    "url": "/img/happy-glasses.png"
+  },
+  {
+    "revision": "4554f4c0d5ec09715e73fbfac8961f51",
+    "url": "/img/friends-hang.jpg"
+  }
+];
